d4f714014128cf203e5d5d39dcf4a039515656bb
Loiiiz <rost.louise@gmail.com>
2018-02-28 12:32:22 +0100

    Added test_heuristic.py

--- test_heuristic.py
+++ test_heuristic.py
@@ -1,11 +1,15 @@
+import sys
+sys.path.append("./utils/")
+
 from amplpy import AMPL
 from typing import NewType
+from getpara import *
 
 # Create AMPL instance
 ampl = AMPL()
 
 ampl.read('model_template.mod')
-#ampl.readData('test_heuristic.dat')
+ampl.readData('2010-08-16_20j_MTC6.dat')
 ampl.setOption('solver','cplex')
 
 # Get sets and parameters from ampl model
@@ -17,27 +21,28 @@ a_disc = ampl.getParameter('a_disc')
 r_disc = ampl.getParameter('r_disc')
 proc_time_disc = ampl.getParameter('proc_time_disc')
 p_j_o_postmach_disc = ampl.getParameter('p_j_o_postmach_disc')
-lambda_mach = ampl.getParameter('lambda_mach')
 
-# Set Parameters to integers
-maxjobs = NewType('maxjobs', int)
-lambda_mach = NewType('lambda_mach', int)
+# Import and convert parameters from .dat-file
+maxjobs = int(getSingleParameter(ampl,'maxjobs'))
+lambda_mach = flexdf2mat(ampl.getParameter('lambda_mach').getValues(),maxjobs,5)
 
-# Set test values
-maxjobs = 10
+# Initialize own parameters and variables
 JOBS_left = list(range(1,maxjobs+1)) # name jobs 1,...,maxjobs
-lambda_mach = [[1 for col in range(5)] for row in range(maxjobs)] # set elements in lambda to 0 or 1
-lambda_mach[0][0] = 0
-lambda_mach[0][4] = 0
-lambda_mach[1][1] = 0
-lambda_mach[2][1] = 0
-lambda_mach[6][3] = 0
-lambda_mach[9][0] = 0
-
+a_disc_list = ampl.getParameter('a_disc').getValues().toList()
+mach_avail_time = [int(a_disc_list[i][1]) for i in range(5)]
+proc_time_disc_list = ampl.getParameter('proc_time_disc').getValues().toList()
+proc_time_disc = [int(proc_time_disc_list[i][1]) for i in range(maxjobs)]
 
 
 # Heuristic
 while (len(JOBS_left) > 0):
-    JOBS_left.pop(0)
-    print(JOBS_left)
-
+    machine = mach_avail_time.index(min(mach_avail_time)) # choose earliest available machine
+    lambda_machine = []
+    for i in JOBS_left:
+        lambda_machine.append(lambda_mach[i-1][machine]) # extract lambda values for chosen machine
+    
+    job_ind = lambda_machine.index(max(lambda_machine))
+    job = JOBS_left[job_ind] # find some job possible on chosen machine
+    JOBS_left.pop(job_ind) # job performed -> remove from JOBS_left
+    
+    mach_avail_time[machine] += proc_time_disc[job_ind] # update available time for machine
