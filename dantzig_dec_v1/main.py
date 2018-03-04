119848da4a7380a422036a3b47b6019ad470b81a
Lunkentuss <peter__hansson@hotmail.com>
2018-03-04 16:27:11 +0100

    Almost fully done with dantzig

--- dantzig_dec_v1/main.py
+++ dantzig_dec_v1/main.py
@@ -12,6 +12,13 @@ def main():
 	ampl.readData('dat.dat')
 	ampl.setOption('solver','cplex')
 
+	# Declare problems
+	ampl.eval('problem lp_dual1: obj_LP_D_RMP, pi, gamma, constraint_d1;')
+	ampl.eval('problem column_generation1: obj_sub, x_sub,constraint_s1,' +
+				'constraint_s1, constraint_s3;')
+	ampl.eval('problem rmp1: obj_rmp, tau, constraint_rmp1, constraint_rmp2;')
+	ampl.eval('problem rmp1_bin: obj_rmp_bin, tau_bin, constraint_rmp1_bin, constraint_rmp2_bin;')
+
 	# Get nbr of machines and the set 
 	mach_list = set2list(ampl,'K_mach_RESOURCES')
 	nbr_of_machines = len(mach_list)
@@ -25,7 +32,7 @@ def main():
 
 	# Set A[j] and B[j]
 	A = [1 for x in range(nbr_of_jobs)]
-	B = [1 for x in range(nbr_of_jobs)]
+	B = [0 for x in range(nbr_of_jobs)]
 	setParamOfSingleSet(ampl,'JOBS','A',A)
 	setParamOfSingleSet(ampl,'JOBS','B',B)
 
@@ -50,7 +57,6 @@ def main():
 
 	#print(ampl.getParameter('x_heur').getValues().toDict())
 
-
 	#print(heur_dict)
 	#print('L_len := ' + str(getSingleParameter(ampl,'L_len')))
 
@@ -59,32 +65,79 @@ def main():
 
 
 	# Loop for column generation
-	max_iterations = 2;
-	l = 2;
-	ampl.eval('let L_len := 2;')
-	while(1):
+	max_iterations = 100;
+	l = 1;
+	while(True):
 		# Debuggers:
 		#print('L_len := ' + str(getSingleParameter(ampl,'L_len')))
 
 		# Termination criteria == 1 (close enough criteria)
-		if(1 != 1 or l >= max_iterations): 
+		if(l > max_iterations): 
 			break;
 
-		# Solve dual RMP
+		# Solve RMP (pessimistic bound)------------------------------
+		# New problem decleration because strange problem
 
-		# Solve column generation problem and update x_bar
+		ampl.eval('solve rmp1;')
+		#ampl.eval('display tau;')
 
-		# Solve RMP (pessimistic bound)
+		print('== obj_rmp: ' + repr(ampl.getObjective('obj_rmp').value()))
+
+		#ampl.eval('display pi;')
+		#ampl.eval('display gamma;')
 
 		# Optimistic bound??????
-	
+
+		# Solve dual RMP --------------------------------------------
+		ampl.eval('solve lp_dual1;')
+
 		# Increase l
-		l = l + 1;
+		l = l + 1
 		ampl.eval('let L_len := ' + repr(l) + ';')
 
+		# Solve column generation problem and update x_bar-----------
+		reduced_cost = column_gen(ampl,mach_list)
+		
+		if(isAllPositive(reduced_cost)):
+			break
+
+
 	# Solve RMP with binary constraint
+	print(l)
+	ampl.eval('solve rmp1_bin;')
 
 
+def column_gen(ampl,mach_list):
+	red_cost = [0 for x in range(len(mach_list))]
+
+	for i in range(len(mach_list)):
+		ampl.eval('reset data mach_k;')
+		ampl.eval('data ; set mach_k := ' + mach_list[i] + ';')
+		#print(set2list(ampl,'mach_k'))
+		ampl.eval('solve column_generation1;')
+		red_cost[i] = ampl.getObjective('obj_sub').value()
+
+		# Eval x_bar_sum_u
+		ampl.eval('let {l in L_len..L_len, j in JOBS, k in mach_k}' +
+			  	'x_bar_sum_u[l,j,k]  := sum{u in TIME}(x_sub[j,k,u]);')
+		# Eval x_bar_sum_u_star
+		ampl.eval('let {l in L_len..L_len, k in mach_k}' +
+			  	'x_bar_sum_u_star[l,k]  := sum{u in TIME, j in JOBS}' + 
+				'((A[j]*(u+p_j_o_postmach_disc[j])+B[j]' + 
+				'*max(u+p_j_o_postmach_disc[j]-d_disc[j],0))*x_sub[j,k,u]);')
+
+	#print(red_cost)
+	return red_cost
+
+def isAllPositive(array):
+	"""	Returns true if an array
+	has only non-negative values
+	"""
+
+	for i in range(len(array)):
+		if(array[i] < 0):
+			return False
+	return True 
 
 if __name__ == '__main__':
 	main()
--- dantzig_dec_v1/mod.mod
+++ dantzig_dec_v1/mod.mod
@@ -113,7 +113,7 @@ maximize obj_LP_D_RMP:
 	
 #---------- Constraints --------------------------------
 subject to constraint_d1 {l in L,k in K_mach_RESOURCES}:
-	sum{j in JOBS}(x_bar_sum_u[l,j,k] * pi[j]) + gamma[k]  <= x_bar_sum_u_star[l,k]; # Summation problem?
+	sum{j in JOBS}(x_bar_sum_u[l,j,k] * pi[j]) + gamma[k]  <= x_bar_sum_u_star[l,k]; # Summation problem goes where?
 		
 #---------- Problem ------------------------------------
 problem lp_dual: obj_LP_D_RMP, pi, gamma, constraint_d1;
@@ -152,3 +152,18 @@ subject to constraint_rmp2 {k in K_mach_RESOURCES}: sum{l in L}(tau[k,l]) == 1;
 
 #---------- Problem ------------------------------------
 problem rmp: obj_rmp, tau, constraint_rmp1, constraint_rmp2;
+
+# ======================= RMP-binary ===================
+# ======================================================
+var tau_bin{K_mach_RESOURCES,1..L_len-1} >= 0;
+
+#---------- Objective function -------------------------
+minimize obj_rmp_bin: sum{l in 1..L_len-1,k in K_mach_RESOURCES}(x_bar_sum_u_star[l,k] * tau[k,l]);
+
+#---------- Constraints --------------------------------
+subject to constraint_rmp1_bin {j in JOBS}: sum{k in K_mach_RESOURCES, l in 1..L_len-1}(x_bar_sum_u[l,j,k] * tau[k,l]) == 1;
+
+subject to constraint_rmp2_bin {k in K_mach_RESOURCES}: sum{l in 1..L_len-1}(tau[k,l]) == 1;
+
+#---------- Problem ------------------------------------
+problem rmp_bin: obj_rmp_bin, tau_bin, constraint_rmp1_bin, constraint_rmp2_bin;
