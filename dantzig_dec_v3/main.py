29893d2d21eb5119933a84ed92f77507b35f8675
Peter Hansson <peter__hansson@hotmail.com>
2018-03-06 11:18:54 +0100

    Added some plots

--- dantzig_dec_v3/main.py
+++ dantzig_dec_v3/main.py
@@ -2,10 +2,16 @@ import matplotlib.pyplot as plt
 from dantzig import dantzig
 
 def main():
-	data_path = 'dat.dat'
-	# tuple(0) := solutions, tuple(1) := ) 
-	tuple_dantzig = dantzig(data_path)
-	plt.plot(tuple_dantzig[1],tuple_dantzig[2])
+    data_path = '../data/dat11.dat'
+    # tuple(0) := solutions, tuple(1) := ) 
+    tuple_dantzig = dantzig(data_path)
+    # TIME:x Upperbound:y
+    #plt.plot(tuple_dantzig[2],tuple_dantzig[1])
+
+    # Iteration:x Upperbound:y
+    plt.plot([x+1 for x in range(len(tuple_dantzig[1]))],tuple_dantzig[1])
+
+    plt.show()
 
 if __name__ == '__main__':
 	main()
